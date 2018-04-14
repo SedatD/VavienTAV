9086ff8580549bb17475d0e56635776987304fb4
SedatD <dilmacsedat@gmail.com>
2018-04-14 18:25:43 +0300

    init

---
+++ app/src/main/java/agency/vavien/vavientav/LogicActivity.java
@@ -0,0 +1,13 @@
+package agency.vavien.vavientav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logic);
+    }
+}
